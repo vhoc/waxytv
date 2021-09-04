d327fe68cb0e1284c3984d575925968622ba6014
Victor H. Olvera <vhocar@gmail.com>
2021-09-04 13:16:43 -0700

    .

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->put( '/play', 'MediaController@play' );
+$router->post( '/play', 'MediaController@play' );
