fbb1e8a4ca23e84bad163a9bc1fa55abbde7a1b5
Victor H. Olvera <vhocar@gmail.com>
2021-09-04 13:45:40 -0700

    .

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -16,8 +16,8 @@ class MediaController extends Controller
     public function play( Request $request )
     {
         //shell_exec( "vlc '" + $request->file + "'" );
-        //return response->json( $request );
-        shell_exec( "sudo reboot" );
+        return response->json( $request );
+        //shell_exec( "sudo reboot" );
     }
 
     public function test()
