e21549bad58c2343a8573cc560f8f54f93b3ff58
Victor H. Olvera <vhocar@gmail.com>
2021-09-06 11:32:58 -0700

    .

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -16,7 +16,9 @@ class MediaController extends Controller
     public function play( Request $request )
     {
         //shell_exec( "cvlc '". $request->file ."'" );
-        shell_exec( "wget --user= --password=waxytv http://localhost:8080/requests/status.xml?command=in_play&input=" . $request->file );
+        $command = "wget --user= --password=waxytv http://localhost:8080/requests/status.xml?command=in_play&input=" . $request->file;
+        return $command;
+        //shell_exec( "wget --user= --password=waxytv http://localhost:8080/requests/status.xml?command=in_play&input=" . $request->file );
     }
 
     public function playlistClear()
